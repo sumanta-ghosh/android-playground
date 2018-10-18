99e10680360456d84e2712df7524a002561936e5
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-10-18 22:43:37 +0530

    Initilized new project for implicit intents.

---
+++ ImplicitIntents/app/src/main/java/com/implicitintents/codeguy/implicitintents/MainActivity.java
@@ -0,0 +1,39 @@
+package com.implicitintents.codeguy.implicitintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void openLocation(View view) {
+    }
+
+    public void shareText(View view) {
+    }
+}
