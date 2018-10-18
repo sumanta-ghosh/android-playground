753c3c0188339c025f2f5fc79b439615fddb097e
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-10-18 17:33:32 +0530

    implement startActivityForResult

--- TwoActivities/app/src/main/java/com/example/twoactivities/codeguy/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/codeguy/twoactivities/MainActivity.java
@@ -6,17 +6,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int TEXT_REQUEST = 1;
     public static final String EXTRA_MSG = "MSG_FORM_ACTIVITY_ONE";
     private EditText textArea;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textArea = findViewById(R.id.editText_main);
+
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
@@ -24,6 +31,20 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SecondActivity.class);
         String message = textArea.getText().toString();
         intent.putExtra(EXTRA_MSG, message);
-        startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- TwoActivities/app/src/main/java/com/example/twoactivities/codeguy/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/codeguy/twoactivities/SecondActivity.java
@@ -3,9 +3,15 @@ package com.example.twoactivities.codeguy.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private String sReply;
+    public static final String EXTRA_REPLY = "reply_2nd_screen";
+    private EditText vReply;
+    private Intent inReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,15 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MSG);
         TextView txt = findViewById(R.id.text_message);
         txt.setText(message);
+        vReply = findViewById(R.id.replyText);
 
+    }
+
+    public void replyTheMessage(View view) {
+        sReply = vReply.getText().toString();
+        inReply = new Intent();
+        inReply.putExtra(EXTRA_REPLY, sReply);
+        setResult(RESULT_OK,inReply);
+        finish();
     }
 }
