9e737bdafba0ba03f6cebc4e0d124ad5c11356df
Sumanta Kr Ghosh <sumanta.ghosh@infoway.us>
2018-10-27 19:56:27 +0530

    Add toast message.

--- DroidCafe/app/src/main/java/com/droidcafe/codeguy/droidcafe/MainActivity.java
+++ DroidCafe/app/src/main/java/com/droidcafe/codeguy/droidcafe/MainActivity.java
@@ -42,16 +42,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_order) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // Do nothing
         }
-
         return super.onOptionsItemSelected(item);
     }
 
