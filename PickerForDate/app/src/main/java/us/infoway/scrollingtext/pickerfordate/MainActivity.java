948923251e50497e10304dcf889b1158c0d6974b
Sumanta Kr Ghosh <sumanta.ghosh@infoway.us>
2018-10-31 17:21:17 +0530

    Implement the date picker

--- PickerForDate/app/src/main/java/us/infoway/scrollingtext/pickerfordate/MainActivity.java
+++ PickerForDate/app/src/main/java/us/infoway/scrollingtext/pickerfordate/MainActivity.java
@@ -1,7 +1,10 @@
 package us.infoway.scrollingtext.pickerfordate;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this, "Date: " + dateMessage, Toast.LENGTH_SHORT).show();
     }
 }
