3054a53d78446c4f6ae87e1212c1ac1a5a58800d
Sumanta Kr Ghosh <sumanta.ghosh.mit@gmail.com>
2018-11-11 01:54:13 +0530

    add tabExperiment apps

---
+++ TabExperiment/app/src/main/java/com/droidcafe/codeguy/tabexperiment/PagerAdapter.java
@@ -0,0 +1,49 @@
+package com.droidcafe.codeguy.tabexperiment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.droidcafe.codeguy.tabexperiment.TabFragment1;
+import com.droidcafe.codeguy.tabexperiment.TabFragment2;
+import com.droidcafe.codeguy.tabexperiment.TabFragment3;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new TabFragment1();
+            case 1:
+                return new TabFragment2();
+            case 2:
+                return new TabFragment3();
+            case 3:
+                return new TabFragment3();
+            case 4:
+                return new TabFragment3();
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
