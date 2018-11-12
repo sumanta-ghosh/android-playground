705af032fb57d72078e533e91754be97032101a4
Sumanta Kr Ghosh <sumanta.ghosh@infoway.us>
2018-11-12 21:06:25 +0530

    Add item view RecyclerView

--- RecyclerView/app/src/main/java/in/sumanta/navigationdrawer/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/in/sumanta/navigationdrawer/recyclerview/MainActivity.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Put initial data into the word list.
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
     }
 
     @Override
