a849f99f367436a2c5ea263b572225c7d920f9b4
Sumanta Kr Ghosh <sumanta.ghosh@infoway.us>
2018-11-14 20:56:38 +0530

    Modify wordlist

--- RecyclerView/app/src/main/java/in/sumanta/navigationdrawer/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/in/sumanta/navigationdrawer/recyclerview/MainActivity.java
@@ -1,10 +1,14 @@
 package in.sumanta.navigationdrawer.recyclerview;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +18,10 @@ import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private final LinkedList<String> mWordList = new LinkedList<>();
+    private final LinkedList<String> mWordListOriginal = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private Parcelable recyclerViewState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +34,32 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                int wordListSize = mWordList.size();
+                // Add a new word to the wordList.
+                mWordList.addLast("+ Word " + wordListSize);
+                // Notify the adapter, that the data has changed.
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                // Scroll to the bottom.
+                mRecyclerView.smoothScrollToPosition(wordListSize);
             }
         });
 
         // Put initial data into the word list.
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 100; i++) {
             mWordList.addLast("Word " + i);
+            mWordListOriginal.addLast("Word " + i);
         }
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerViewState = mRecyclerView.getLayoutManager().onSaveInstanceState();
     }
 
     @Override
@@ -52,7 +77,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_reset) {
+            // Restore state
+            mRecyclerView.getLayoutManager().onRestoreInstanceState(recyclerViewState);
             return true;
         }
 
