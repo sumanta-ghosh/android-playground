f3300f7682ca442c14a90b0ad13238314a589ae6
Sumanta Kr Ghosh <sumanta.ghosh@infoway.us>
2018-11-15 20:02:02 +0530

    Implement click function for scorekeeper apps

--- Scorekeeper/app/src/main/java/in/sumanta/scorekeeper/MainActivity.java
+++ Scorekeeper/app/src/main/java/in/sumanta/scorekeeper/MainActivity.java
@@ -2,12 +2,49 @@ package in.sumanta.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mScore1 = 0;
+    private int mScore2 = 0;
+
+    private TextView mScoreText1;
+    private TextView mScoreText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+    }
+
+    public void decreaseScore(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void increaseScore(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 }
