f7391c7397fcf057b0578e4037c21f2dd40f9d75
Sumanta Kr Ghosh <sumanta.ghosh@infoway.us>
2018-11-19 19:05:16 +0530

    Complete Homework12 app

--- Homework12/app/src/main/java/in/sumanta/homework_12/MainActivity.java
+++ Homework12/app/src/main/java/in/sumanta/homework_12/MainActivity.java
@@ -2,12 +2,35 @@ package in.sumanta.homework_12;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView battery;
+    private int batteryLevel = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        battery = findViewById(R.id.imageViewBattery);
+    }
+
+    public void inCrease(View v) {
+        if (batteryLevel < 3) {
+            batteryLevel++;
+            updateBatteryImg(batteryLevel);
+        }
+    }
+
+    public void deCrease(View v) {
+        if (batteryLevel > 0) {
+            batteryLevel--;
+            updateBatteryImg(batteryLevel);
+        }
+    }
+
+    private void updateBatteryImg(Integer level) {
+        battery.setImageLevel(level);
     }
 }
