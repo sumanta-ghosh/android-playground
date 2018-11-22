f262918a76781366637a54adf7df9554ac157cce
Sumanta Kr Ghosh <sumanta.ghosh@infoway.us>
2018-11-22 21:55:39 +0530

    Modify Async apps

--- SimpleAsyncTask/app/src/main/java/in/sumanta/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/in/sumanta/simpleasynctask/MainActivity.java
@@ -3,15 +3,36 @@ package in.sumanta.simpleasynctask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Restore TextView if there is a savedInstanceState
+        mTextView = findViewById(R.id.textView1);
+        if (savedInstanceState != null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
     }
 
     public void startTask(View view) {
+        // Put a message in the text view
+        mTextView.setText("Napping...");
+
+        // Start the AsyncTask.
+        new SimpleAsyncTask(mTextView).execute();
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+
 }
